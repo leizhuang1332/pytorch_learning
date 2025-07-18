7830072f41057d2c32903caa64ff92aaeaab62b0
leizhuang <lorcan.lei@jtexpress.com>
2025-07-18 09:16:56 +0800

    Quickstart 优化

--- 1_Quickstart.py
+++ 1_Quickstart.py
@@ -120,8 +120,8 @@ def test(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-def main(eval=False):
-    if (eval):
+def main(predict_=False, epochs=5):
+    if predict_:
         # 加载模型
         model_eval = NeuralNetwork().to(device)
         model_eval.load_state_dict(torch.load("model.pth", weights_only=True))
@@ -146,7 +146,6 @@ def main(eval=False):
                 predicted, actual = classes[pred[0].argmax(0)], classes[y]
                 print(f'Predicted: "{predicted}", Actual: "{actual}"')
     else:
-        epochs = 2
         for t in range(epochs):
             print(f"Epoch {t + 1}\n-------------------------------")
             train(train_dataloader, model, loss_fn, optimizer)
